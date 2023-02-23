231d45cc026cef55920497bf6f5037d042936c30
sarra-a <126196866+sarra-a@users.noreply.github.com>
2023-02-23 14:06:13 -0500

    Add files via upload

---
+++ Lab3/script.js
@@ -0,0 +1,66 @@
+function validateForm(){
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+
+    if (!username){
+        console.log("Error: enter username.");
+        return false;
+    }else if (!/^[a-zA-Z0-9]+$/.test(username)){
+        console.log("Username can only contain letters and numbers");
+        return false;
+    }
+
+    if (!email){
+        console.log("Error: please enter an email address");
+        return false;
+    }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        console.log("Invalid email format. Must contain '@' character");
+        return false;
+    }
+
+    if (!password){
+        console.log("Error: please enter a password");
+        return false;
+    }else if (password.length < 8){
+        console.log("Password must be at least 8 characters long");
+        return false;
+    }
+
+    console.log("Form submitted successfully!");
+    return true;
+}
+
+    const products = ["T-shirt", "Bra", "Pants", "Shorts", "Underwear", "Headband", "Earrings", "Hat", "Sneakers", "Heels"];
+
+    const searchField = document.getElementById("search");
+    const productList = document.getElementById("products");
+
+
+    function searchProducts(query){
+
+        const matchProducts = products.filter((product)=>{
+            return product.includes(query)
+        })
+        return matchProducts;
+    }
+
+    function updateProductList(){
+        const query = searchField.value;
+        const products = searchProducts(query);
+
+        productList.innerHTML = "";
+
+        for( let i = 0; i < products.length; i++){
+            const product = products[i];
+            const item = document.createElement("li");
+
+            item.textContent = product;
+            productList.appendChild(item);
+        }
+    }
+
+    searchField.addEventListener("change", updateProductList);
+    
+    updateProductList();
